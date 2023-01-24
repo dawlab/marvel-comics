0d9d3b26dc49b0f3da47869a01b4994968c6a59b
Dawid <dawidlabno@outlook.com>
2023-01-24 13:25:37 +0100

    - Add DetailViewController

--- MarvelComics/Controllers/DetailViewController.swift
+++ MarvelComics/Controllers/DetailViewController.swift
@@ -13,11 +13,21 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var comicImageView: UIImageView!
     
+    @IBOutlet weak var smallDescView: UIView!
+    
+    @IBOutlet weak var grabber: UIView!
+    
     var imgUrl: URL?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         comicImageView.sd_setImage(with: imgUrl)
-
+        
+        navigationItem.backBarButtonItem?.image = UIImage(systemName: "arrow.backward")
+        navigationItem.backButtonDisplayMode = .minimal
+        
+        smallDescView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        smallDescView.layer.cornerRadius = 25
+        grabber.layer.cornerRadius = 5
     }
 }
