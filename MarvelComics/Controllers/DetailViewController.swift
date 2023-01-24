b809a39189c846edecad76ea6d9038bb15c784e2
Dawid <dawidlabno@outlook.com>
2023-01-24 11:35:09 +0100

    - Add DetailViewController

--- MarvelComics/Controllers/DetailViewController.swift
+++ MarvelComics/Controllers/DetailViewController.swift
@@ -20,15 +20,4 @@ class DetailViewController: UIViewController {
         comicImageView.sd_setImage(with: imgUrl)
 
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
